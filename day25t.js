fc8e9ab25a0e72a41e32ec0091359759859e4411
Nirpa Rokaya <rokayanirpa618@gmail.com>
2025-05-25 07:54:22 +0545

    feat: js practice

--- day25t.js
+++ day25t.js
@@ -1,1 +1,28 @@
-console.log("May 25th")
+const Userdata=[
+    {id:1, name:"Nirpa",age:19,gender:"male", hasPassOut:true, occupation:"Intern"},
+    {id:2, name:"Awasthi",age:21,gender:"male", hasPassOut:true, occupation:undefined},
+    {id:3, name:"Jalan",age:19,gender:"male", hasPassOut:true, occupation:undefined},
+    {id:4, name:"Shital",age:19,gender:"female", hasPassOut:true, occupation:undefined},
+    {id:5, name:"Ishwor",age:19,gender:"", hasPassOut:false, occupation:undefined},
+    {id:6, name:"Ishwor",age:19,gender:"", hasPassOut:false, occupation:undefined},
+    {id:7, name:"Ishwor",age:19,gender:"", hasPassOut:false, occupation:undefined},
+]
+
+const response={
+    status:200,
+    message:"Data fetched successfully",
+    data:Userdata
+}
+const getUserName = ()=>{
+    const APIStatus=response.status
+    if(APIStatus === 200){
+        const result = response.data
+        .filter((user)=>user.occupation)
+        .map((user)=>user.name)
+        return result;
+
+    }else{
+
+    }
+}
+console.log(getUserName());
